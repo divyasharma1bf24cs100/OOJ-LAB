a2c633f83050781fedf76a875d1d99b163ca5d84
divyasharma1bf24cs100 <divyasharma.cs24@bmsce.ac.in>
2025-12-14 22:55:48 +0530

    Create Internals.java

---
+++ Lab5/CIE/Internals.java
@@ -0,0 +1,9 @@
+package CIE;
+public class Internals{
+    public int marks[] = new int[5];
+    public Internals(int internalmarks[]){
+        for(int i=0;i<5;i++){
+            marks[i]=internalmarks[i];
+        }
+    }
+}
